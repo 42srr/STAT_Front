6a88c546c2e622807aa3c2bcf6ca7cc2ef11df11
조재영 <68009715+joejaeyoung@users.noreply.github.com>
2024-07-23 22:36:57 +0900

    Update reservationPage.jsx
    Delete : blank line

--- src/routes/reservationPage.jsx
+++ src/routes/reservationPage.jsx
@@ -41,7 +41,6 @@ export default function ReservationPage() {
             className="continue-button">
             Continue
           </button>
-          
         </div>
       </main>
     </div>
