660ae6ca86e4d2e4079ca51ac4e6c3c24634fc3f
hyerimYeom <hyerim.yeom@kakao.com>
2024-10-07 00:33:49 +0900

    [front] info페이지 수정
    - tailwind 버전 변경 : 3.4.3 -> 3.4.13

--- src/routes/infoPage.jsx
+++ src/routes/infoPage.jsx
@@ -1,14 +1,52 @@
 import SideBar from "../components/SideBar.jsx";
+import "../main.css";
 
 export default function InfoPage() {
+  // api
+
+  // 반응형
+  // tailwind로 확인
   return (
-    <>
+    <div className="flex flex-row">
       <SideBar />
-      <div>
-        <div>이미지</div>
-        <div>이름 코알리숑 LV</div>
-      </div>
-      <div>보너스 프리즈</div>
-    </>
+      <main className="box-border flex flex-col w-screen gap-4 py-8 pl-48 pr-56">
+        {/* background color 바뀔 수 있도록 custom 추가 / props 입력 */}
+        <div className="flex justify-around flex-none px-20 py-4 mt-8 mb-2 border-2 border-none shadow-md bg-slate-100 rounded-2xl">
+          <div className="px-2">
+            {/* 이미지가 없을 때 (가 있나요..?) */}
+            <img
+              className="w-48 h-48 mx-auto rounded-full"
+              src="https://dummyimage.com/600"
+              alt="설명 텍스트"
+            />
+            {/* 이미지가 있을때 
+            <img className="w-32 h-32 mx-auto rounded-full" src="https://dummyimage.com/300" alt="설명 텍스트" />
+            */}
+          </div>
+          <div className="content-center">
+            <div>이름</div>
+            <div>코알리숑</div>
+            <div>LV</div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3 py-4 justify-content">
+          <div className="flex flex-row justify-between py-3 border-2 border-slate-200 px-7 rounded-2xl hover:shadow-sm">
+            <div className="px-4 py-2 font-semibold">보너스 프리즈</div>
+            <div className="px-4 py-2">1125일</div>
+            <button className="px-4 py-2 font-bold text-white rounded-full bg-slate-500 hover:bg-violet-500">사용하기</button>
+          </div>
+          <div className="flex flex-row justify-between py-3 border-2 border-slate-200 px-7 rounded-2xl hover:shadow-sm">
+            <div className="px-4 py-2 font-semibold">Sample1</div>
+            <div className="px-4 py-2">sample1</div>
+            <button className="px-4 py-2 font-bold text-white rounded-full bg-slate-500 hover:bg-violet-500">Use</button>
+          </div>
+          <div className="flex flex-row justify-between py-3 border-2 border-slate-200 px-7 rounded-2xl hover:shadow-sm">
+            <div className="px-4 py-2 font-semibold">Sample2</div>
+            <div className="px-4 py-2">sample2</div>
+            <button className="px-4 py-2 font-bold text-white rounded-full bg-slate-500 hover:bg-violet-500">Use</button>
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
