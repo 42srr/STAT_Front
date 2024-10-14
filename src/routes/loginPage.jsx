42510d83ef6f837a69da72bd4d10b0d687183f6d
mbp14 <tm1361@naver.com>
2024-10-14 16:00:57 +0900

    [feat] auth 구현(임시) - 버튼을 눌러 인증 및 토큰 발급 기능  추가 - 토큰을 첨부하여 서버에 요청하는 기능 추가 [UI] 불필요한 UI 제거 - ID 및 PW 입력란 제거

--- src/routes/loginPage.jsx
+++ src/routes/loginPage.jsx
@@ -3,61 +3,145 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+let Flexs = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+let Logo = styled.div`
+  margin: auto;
+`;
+let Fields = styled.input`
+  background: #d9d9d9;
+  width: 10rem;
+  margin: auto;
+  margin-top: 1rem;
+`;
+let Buttons = styled.button`
+  background: #d9d9d9;
+  margin: auto;
+  margin-top: 1rem;
+  width: 10rem;
+`;
+
 export default function loginPage() {
-  const [authCode, setAuthCode] = useState("");
+  const navigate = useNavigate();
+  const [accessToken, setAccesToken] = useState("");
+  const [refreshToken, setRefreshToken] = useState("");
+  // local용 URL
+  const toGetAuthCodeUrl =
+    "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-598ae18e7a326adf87c4c13c715a91675c6b68458bb4082e24e297616ebd98d4&redirect_uri=http%3A%2F%2Flocalhost%3A5173&response_type=code";
   useEffect(() => {
-    const search = window.location?.search;
-    const data = search.split("code=")[1];
-    console.log(search);
-    console.log(data);
-    setAuthCode(data);
+    const search = window.location?.search.split("code=")[1];
+    if (search) {
+      getAuth(search);
+    }
   }, []);
-  function getAuth() {
+  async function getAuth(code) {
     axios
-      .get("http://118.67.134.143:8080/login", { code: authCode })
+      .get("http://118.67.134.143:8080/login", { params: { code: code } })
       .then((res) => {
-        console.log(res);
+        console.log("accessToken:");
+        console.log(res.data.accessToken);
+        console.log("refreshToken:");
+        console.log(res.data.refreshToken);
+        setAccesToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
       });
   }
-  let navigate = useNavigate();
-  let Flexs = styled.div`
-    display: flex;
-    flex-direction: column;
-  `;
+  async function getRefresh() {
+    axios
+      .post("http://118.67.134.143:8080/refresh", {
+        refreshToken: refreshToken,
+      })
+      .then((res) => {
+        console.log("accessToken:");
+        console.log(res.data.accessToken);
+        console.log("refreshToken:");
+        console.log(res.data.refreshToken);
+        setAccesToken(res.data.accessToken);
+        setRefreshToken(res.data.refreshToken);
+      });
+  }
+  function getProjects() {
+    axios
+      .get("http://118.67.134.143:8080/projects/yutsong", {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
+  function getRankWallet() {
+    axios
+      .get("http://118.67.134.143:8080/ranking/wallet", {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
+  function getLevels() {
+    axios
+      .get("http://118.67.134.143:8080/levels", {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
+  function getUsers() {
+    axios
+      .get("http://118.67.134.143:8080/users", {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
 
-  let Logo = styled.div`
-    margin: auto;
-  `;
-  let Fields = styled.input`
-    background: #d9d9d9;
-    width: 10rem;
-    margin: auto;
-    margin-top: 1rem;
-  `;
-  let Buttons = styled.button`
-    background: #d9d9d9;
-    margin: auto;
-    margin-top: 1rem;
-    width: 10rem;
-  `;
   return (
     <>
       <Flexs>
         <Logo>42경산로고</Logo>
-        <Fields></Fields>
-        <Fields></Fields>
         <Buttons>
-          {/* <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-c8ae2f770fbf4c851ea2d700c1b7de23af5c008cd24837eac2b163f3c0e24c67&redirect_uri=http%3A%2F%2Flocalhost%3A5173&response_type=code"> */}
-          <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-598ae18e7a326adf87c4c13c715a91675c6b68458bb4082e24e297616ebd98d4&redirect_uri=http%3A%2F%2F118.67.134.143%3A80&response_type=code">
-            auth test btn
-          </a>
+          <a href={toGetAuthCodeUrl}>auth test btn</a>
         </Buttons>
         <Buttons
           onClick={() => {
-            getAuth();
+            getRankWallet();
           }}
         >
-          test
+          Rank/Wallet
+        </Buttons>
+        <Buttons
+          onClick={() => {
+            getProjects();
+          }}
+        >
+          Projects
+        </Buttons>
+
+        <Buttons
+          onClick={() => {
+            getLevels();
+          }}
+        >
+          Levels
+        </Buttons>
+        <Buttons
+          onClick={() => {
+            getUsers();
+          }}
+        >
+          Users
+        </Buttons>
+        <Buttons
+          onClick={() => {
+            getRefresh();
+          }}
+        >
+          Refresh
         </Buttons>
         <Buttons
           onClick={() => {
