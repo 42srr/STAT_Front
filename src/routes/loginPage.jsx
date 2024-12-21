f9b99b126a6d017306502252dc6da5e7c39c858f
mbp14 <tm1361@naver.com>
2024-12-21 19:39:52 +0900

    [fix] 버튼수정3

--- src/routes/loginPage.jsx
+++ src/routes/loginPage.jsx
@@ -191,7 +191,7 @@ export default function LoginPage({
           </Text>
           <Buttons>
             <a href={toGetAuthCodeUrl}>
-              <span style={{ color: "grey" }}>Login with </span>
+              <span style={{ color: "white" }}>Login with </span>
               <b>42GS</b>
             </a>
           </Buttons>
