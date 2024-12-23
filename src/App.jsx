b8e0960c110373d39c4500e6a1ece99dfc3a093c
mbp14 <tm1361@naver.com>
2024-12-23 14:07:21 +0900

    [feat] 보안 기능 추가 - 로그인이 필요한 페이지로 라우팅 시 액세스 토큰 요구

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import WebPrivacy from "./routes/webprivacy.jsx";
 import AppPrivacy from "./routes/app.privacy.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 function App() {
   const [accessToken, setAccessToken] = useState("");
@@ -33,20 +34,57 @@ function App() {
         <Route
           path="/main"
           element={
-            <MainPage
-              setAccessToken={setAccessToken}
-              setRefreshToken={setRefreshToken}
-              accessToken={accessToken}
-              refreshToken={refreshToken}
-              intraId={intraId}
-            />
+            <ProtectedRoute accessToken={accessToken}>
+              <MainPage
+                setAccessToken={setAccessToken}
+                setRefreshToken={setRefreshToken}
+                accessToken={accessToken}
+                refreshToken={refreshToken}
+                intraId={intraId}
+              />
+            </ProtectedRoute>
           }
         />
-        <Route path="/rank" element={<RankPage />} />
-        <Route path="/set" element={<SetPage />} />
-        <Route path="/reservation" element={<ReservationPage />} />
-        <Route path="/reservationDetail" element={<ReservationDetail />} />
-        <Route path="/info" element={<InfoPage />} />
+        <Route
+          path="/rank"
+          element={
+            <ProtectedRoute accessToken={accessToken}>
+              <RankPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/set"
+          element={
+            <ProtectedRoute accessToken={accessToken}>
+              <SetPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/reservation"
+          element={
+            <ProtectedRoute accessToken={accessToken}>
+              <ReservationPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/reservationDetail"
+          element={
+            <ProtectedRoute accessToken={accessToken}>
+              <ReservationDetail />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/info"
+          element={
+            <ProtectedRoute accessToken={accessToken}>
+              <InfoPage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/webprivacy" element={<WebPrivacy />} />
         <Route path="/appprivacy" element={<AppPrivacy />} />
       </Routes>
