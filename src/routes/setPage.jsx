2abc47da3f5c23f750e3be491de32411528f00eb
mbp14 <tm1361@naver.com>
2024-12-30 13:35:47 +0900

    [feat] 문의하기 버튼 기능 구현

--- src/routes/setPage.jsx
+++ src/routes/setPage.jsx
@@ -1,5 +1,6 @@
 import SideBar from "../components/SideBar.jsx";
 import "../main.css";
+import { useNavigate } from "react-router-dom";
 
 // import SideBar from "../components/SideBar.jsx";
 
@@ -31,16 +32,27 @@ export default function SetPage() {
         <div className="flex flex-col gap-3 py-4 justify-content">
           <div className="flex flex-row justify-between py-3 border-2 border-slate-200 px-7 rounded-2xl hover:shadow-sm">
             <div className="px-4 py-2 font-semibold">문의하기</div>
-            <button className="px-4 py-2 font-bold text-white rounded-full bg-slate-500 hover:bg-violet-500">문의하기</button>
+            <button
+              onClick={() => {
+                window.open("https://forms.gle/wpnkJwC8CRqUU4ePA", "_blank");
+              }}
+              className="px-4 py-2 font-bold text-white rounded-full bg-slate-500 hover:bg-violet-500"
+            >
+              문의하기
+            </button>
           </div>
           <div className="flex flex-row justify-between py-3 border-2 border-slate-200 px-7 rounded-2xl hover:shadow-sm">
             <div className="px-4 py-2 font-semibold">계정</div>
             <div>
-              <button className="px-4 py-2 font-bold text-white rounded-full bg-slate-500 hover:bg-violet-500">로그아웃</button>
+              <button className="px-4 py-2 font-bold text-white rounded-full bg-slate-500 hover:bg-violet-500">
+                로그아웃
+              </button>
             </div>
           </div>
           <div className="mx-auto py-3">
-              <button className="px-4 py-2 text-gray-300 rounded-full hover:outline outline-slate-200">회원탈퇴</button>
+            <button className="px-4 py-2 text-gray-300 rounded-full hover:outline outline-slate-200">
+              회원탈퇴
+            </button>
           </div>
         </div>
       </main>
